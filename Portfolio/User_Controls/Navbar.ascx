7455331fc5850e1da5e02b0c6be3957f0036ec1a
akhil1095 <Akhil.Thakkar@MyGeorgian.ca>
2016-05-30 21:59:43 -0500

    Added method for active page and changed the URL for navbar

--- Portfolio/User_Controls/Navbar.ascx
+++ Portfolio/User_Controls/Navbar.ascx
@@ -14,11 +14,11 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="Default.aspx"><i class="fa fa-home fa-lg"></i> Home</a></li>
-                <li><a href="Services.aspx"><i class="fa fa-cogs fa-lg"></i> Services</a></li>
-                <li><a href="Products.aspx"><i class="fa fa-cubes fa-lg"></i> Products</a></li>
-                <li><a href="AboutUs.aspx"><i class="fa fa-users fa-lg"></i> About Us</a></li>
-                <li><a href="Contacts.aspx"><i class="fa fa-phone-square fa-lg"></i> Contact Us</a></li>
+                <li id="home" runat="server"><a href="Default.aspx"><i class="fa fa-home fa-lg"></i> Home</a></li>
+                <li id="services" runat="server"><a href="Services.aspx"><i class="fa fa-cogs fa-lg"></i> Services</a></li>
+                <li id="products" runat="server"><a href="Products.aspx"><i class="fa fa-cubes fa-lg"></i> Products</a></li>
+                <li id="aboutus" runat="server"><a href="AboutUs.aspx"><i class="fa fa-users fa-lg"></i> About Us</a></li>
+                <li id="contact" runat="server"><a href="Contacts.aspx"><i class="fa fa-phone-square fa-lg"></i> Contact Us</a></li>
 
             </ul>
         </div>
---
+++ Portfolio/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/**
+ * @author: Akhil Thakkar
+ * @date: 30th May */
+namespace Portfolio
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AddActivePage();
+        }
+        /**
+         * This method will add css class of "active" to items related to
+         * navigatory links of every page
+         * 
+         * @method AddActivePage
+         * @return {void} */
+        private void AddActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Products":
+                    products.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+                case "About Us":
+                    aboutus.Attributes.Add("class", "active");
+                    break;
+                case "Contacts":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
